d9b6ae29a292c81667ecfebc62f51b12057dd2ac
SMBolla <saimounikabolla@gmail.com>
2022-02-10 10:43:02 +0530

    first

---
+++ task1.py
@@ -0,0 +1,33 @@
+''' PRACTICE 
+COMMENTS
+VARIABLES
+DATATYPES
+TYPE CONVERSATIONS'''
+
+from typing import Mapping
+
+
+a= 10
+name= 'mounika'
+#assigning a variable name to a value 
+#types of datatypes 
+# Numeric-- integer,float,complex) 
+# Sequence-- string ' ', list[ ] , tuple ( )
+#dictionary{ element:key}
+#Mapping
+#set  {}
+
+rajesh= 102202 #int
+mounika=10.34 #float
+sandy= 3+2j #complex
+harika= 'string'
+c= [1,2,3,2] #list
+d=(2,4,5,6) #tuple
+krishna= True #boolean
+print(type(krishna)) #to know datatype use function  type()
+
+#type conversions
+'''
+* implicit this is automatic output where no data is lost
+* explicit - forcibilly converting '''
+print(int(mounika)) #explicit
